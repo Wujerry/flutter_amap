b1379349a14f6c6c45509a6f1b112f210aa88ce2
JerryWu <wujerry116@163.com>
2019-03-13 15:59:16 +0800

    upgrade to AndroidX

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_amap/flutter_amap.dart';
 import 'package:flutter_amap_example/second_page.dart';
 
 void main(){
-  FlutterAmap.setApiKey("119245a2f777ece25b8aaf89421e6f24");
+  FlutterAmap.setApiKey("0787b16ca4fd97815b1d354571e9f9c1");
   runApp( MaterialApp(
     home:  MyApp(),
     navigatorObservers: [new AMapNavigatorObserver()],
@@ -88,64 +88,60 @@ class _MyAppState extends State<MyApp> {
         ],
       ),
       body:  Container(
-//         child:  Column(
+        child:  Column(
 
-//           children: <Widget>[
-//              SizedBox(
-//               height: 50.0,
-//               child:  Text("第一个地图: $first"),
-//             ), SizedBox(
-//               height: 50.0,
-//               child:  Text("第二个地图: $second" ),
-//             ),
-//              RaisedButton(onPressed: (){
-//                 _showFirst = ! _showFirst;
-//                 setState(() {
+          children: <Widget>[
+             SizedBox(
+              height: 50.0,
+              child:  Text("第一个地图: $first"),
+            ), SizedBox(
+              height: 50.0,
+              child:  Text("第二个地图: $second" ),
+            ),
+             RaisedButton(onPressed: (){
+                _showFirst = ! _showFirst;
+                setState(() {
 
-//                 });
-//             },child:  Text("测试移除第一个"),),
-//             _showFirst ?  Expanded(child:
-//             AMapView(
-//                 key: _key0,
-//                 onLocationChange: (Location location){
-//                   setState(() {
-//                     first = location;
-//                   });
-//                 },
-//                 locateOnce: true,
-// //                centerCoordinate:  LatLng(60.9242, 116.3979),
-//                 zoomLevel: 13.0,
-//                 mapType: MapType.standard,
-//                 showsUserLocation: true
-//             )):Container(),
-//              Expanded(child:
-//             AMapView(
-//                 onLocationChange: (Location location){
-//                  print('lllllllllll'+location.toString());
-//                 },
-//                 onCameraChange: (Location location){
-//                   print('hahahahahaha');
-//                  print('cccccccccccc' + location.toString());
-//                   // AMapView.poiSearch(LatLng(location.latitude, location.longitude), '', _key0);
-//                 },
-//                 onPoiResult: (r){
-//                  print(r);
-//                 },
-//                 onGeoFenceChange: (status){
-//                  print('ggggggg' + status.toString());
-//                 },
-//                 key:_key1,
-//                 // geoFence: GeoFenceSetting(latLng: LatLng(39.9242, 116.3979),
-//                 //   radius: 100,
-//                 //   key: _key1.toString()
-//                 // ),
-//                 // centerCoordinate:  LatLng(39.9242, 116.3979),
-//                 zoomLevel: 13.0,
-//                 mapType: MapType.standard,
-//                 showsUserLocation: true
-//             )),
-//           ],
-//         ),
+                });
+            },child:  Text("测试移除第一个"),),
+            _showFirst ?  Expanded(child:
+            AMapView(
+                key: _key0,
+                onLocationChange: (Location location){
+                  setState(() {
+                    first = location;
+                  });
+                },
+                locateOnce: true,
+                zoomLevel: 13.0,
+                mapType: MapType.standard,
+                showsUserLocation: true
+            )):Container(),
+             Expanded(child:
+            AMapView(
+                onLocationChange: (Location location){
+                },
+                onCameraChange: (Location location){
+                print(1111111111);
+                  AMapView.poiSearch(LatLng(location.latitude, location.longitude), '', _key1);
+                },
+                onPoiResult: (r){
+                  print(7777777777);
+                },
+                onGeoFenceChange: (status){
+                },
+                key:_key1,
+                geoFence: GeoFenceSetting(latLng: LatLng(39.9242, 116.3979),
+                  radius: 100,
+                  key: _key1.toString()
+                ),
+                centerCoordinate:  LatLng(39.9242, 116.3979),
+                zoomLevel: 13.0,
+                mapType: MapType.standard,
+                showsUserLocation: true
+            )),
+          ],
+        ),
       ),
     );
   }
